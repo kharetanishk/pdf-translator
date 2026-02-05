1828c51c1d9394ee1cf26b2411d56ab2e4e3c982
Tanishk khare <kharetanishk18@gmail.com>
2026-02-05 17:22:03 +0530

    structured the backend

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,1 +1,28 @@
-console.log("Hello World");
+import express from "express";
+import { Request, Response } from "express";
+import cors from "cors";
+
+import dotenv from "dotenv";
+dotenv.config();
+
+const PORT = process.env.PORT || 4000
+const app = express();
+
+// middleware
+app.use(express.json());
+app.use(cors())
+
+//health route 
+app.get("/health", (req:Request , res:Response )=>{
+  return res.status(200).json({
+    message: "pdf transalation api is running"
+  });
+});
+
+//routes 
+
+
+//server
+app.listen(PORT ,()=>{
+  console.log(`SERVER IS RUNNING ON PORT ${PORT}`)
+})
