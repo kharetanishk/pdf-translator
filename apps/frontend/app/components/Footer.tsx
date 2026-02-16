94ee7bc559f2878e140778465580c5511fde36f6
Tanishk khare <kharetanishk18@gmail.com>
2026-02-16 17:10:38 +0530

    updated the footer

--- apps/frontend/app/components/Footer.tsx
+++ apps/frontend/app/components/Footer.tsx
@@ -6,6 +6,9 @@ export default function Footer() {
       <div className="mx-auto max-w-6xl text-center text-sm text-[#D6D6D6]/70">
         © {year} made by Tanishk Khare with ❤️
       </div>
+      <h1 className=" max-w-7xl mx-auto text-center font-black leading-none text-transparent bg-clip-text  bg-gradient-to-b from-[#8c8c8c] via-[#2b2b2b] to-[#010101] opacity-90 text-6xl sm:text-[12rem] md:text-[15rem] -z-[1]">
+        GlobalPDF
+      </h1>
     </footer>
   );
 }
